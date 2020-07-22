8e5c3d2578654286cc0632a457c8e1fd9da0d351
tmicka23 <mickathp42@gmail.com>
2020-07-23 00:20:00 +0200

    test fetch api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import API from './service/API';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    API.get('/projects.json')
+      .then((res) => setData(res.data))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, []);
   return (
     <div className='app'>
       <aside className='app-sidebar'>coucou !</aside>
-      <main className='app-main'>Hello !</main>
+      <main className='app-main'>
+        {loading ? <p>Chargement ...</p> : JSON.stringify(data)}
+      </main>
     </div>
   );
 }
