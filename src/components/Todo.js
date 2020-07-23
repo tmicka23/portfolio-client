993acd59b5d271dcd1e0c6beb71191f2d09f648c
tmicka23 <mickathp42@gmail.com>
2020-07-23 11:59:26 +0200

    return to initial state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,15 @@
 import React from 'react';
-// import useFetch from '../hooks/useFetch';
-
-// const Todo = () => {
-//   const [data, loading, error] = useFetch(`/todos.json`);
-//   return (
-//     <div>
-//       {!error && loading
-//         ? 'Chargement ...'
-//         : data.map((d) => <h3>{d.title}</h3>)}
-//     </div>
-//   );
-// };
+import useFetch from '../hooks/useFetch';
 
 const Todo = () => {
-  return <div>TODO</div>;
+  const [data, loading, error] = useFetch(`/todos.json`);
+  return (
+    <div>
+      {!error && loading
+        ? 'Chargement ...'
+        : data.map((d) => <h3>{d.title}</h3>)}
+    </div>
+  );
 };
 
 export default Todo;
