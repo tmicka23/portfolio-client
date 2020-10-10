953f83f69568462078ce25fb7103d17f94b7ecf4
tmicka23 <mickathp42@gmail.com>
2020-10-10 12:49:24 +0200

    refacto cards css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Card.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Card.css";
 
 const Card = ({ name, date, image, tags, id }) => {
   return (
-    <Link to={`/projet/${id}`} className='card-link'>
-      <div className='mt-card'>
-        <h3 className='mt-card-title'>{name}</h3>
-        <span className='date'>{date}</span>
+    <Link to={`/projet/${id}`} className="mt-card-link">
+      <div className="mt-card">
+        <div className="mt-card-header">
+          <h3 className="mt-card-title">{name}</h3>
+          <span className="mt-card-date">{date}</span>
+        </div>
         {image.url && (
-          <img src={process.env.REACT_APP_API_BASEURL + image.url} alt={name} />
+          <img
+            src={
+              process.env.REACT_APP_API_BASEURL +
+              (process.env.NODE_ENV === "production"
+                ? image.url.substring(1)
+                : image.url)
+            }
+            alt={name}
+          />
         )}
-        <span className='tags'>
-          {tags.length > 0 && tags.map((t) => ` ${t.name}`)}
-        </span>
+        <div className="mt-card-footer">
+          <span className="tags">
+            {tags?.length > 0 && tags?.map((t) => ` ${t.name}`)}
+          </span>
+        </div>
       </div>
     </Link>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import useFetch from '../hooks/useFetch';
-import Card from './Card';
-import './Home.css';
+import React from "react";
+import useFetch from "../hooks/useFetch";
+import Card from "./Card";
+import "./Home.css";
 
 const Home = () => {
-  const [data, loading, error] = useFetch('/projects.json');
+  const [data, loading, error] = useFetch("/projects.json");
   return (
-    <main className='home'>
+    <main className="home">
       <h2>Mes Projets</h2>
       {!error && loading ? (
         <p>Chargement ...</p>
       ) : (
-        <div className='cards'>
-          {data.map((d) => (
+        <div className="cards">
+          {data?.map((d) => (
             <Card key={d.id} {...d} />
           ))}
         </div>
